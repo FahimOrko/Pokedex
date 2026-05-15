e2b1984fa0df50c0518e7b599aada08bc4a70251
Shell <fahimorko4122@gmail.com>
2026-05-15 17:05:48 +0000

    Update in relp test ts

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -1,7 +1,7 @@
 import { cleanInput } from "./repl.js";
 import { describe, expect, test } from "vitest";
 
-describe.each([
+const testCasesForCleanInput = [
   {
     input: "  hello  world  ",
     expected: ["hello", "world"],
@@ -14,17 +14,22 @@ describe.each([
     input: "  Trallalla  Land  ",
     expected: ["trallalla", "land"],
   },
-])("cleanInput($input)", ({ input, expected }) => {
-  test(`Expected: ${expected}`, () => {
-    const actual = cleanInput(input);
+];
 
-    // The `expect` and `toHaveLength` functions are from vitest
-    // they will fail the test if the condition is not met
-    expect(actual).toHaveLength(expected.length);
-    for (const i in expected) {
-      // likewise, the `toBe` function will fail the test
-      // if the values are not equal
-      expect(actual[i]).toBe(expected[i]);
-    }
-  });
-});
+describe.each(testCasesForCleanInput)(
+  "cleanInput($input)",
+  ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+      const actual = cleanInput(input);
+
+      // The `expect` and `toHaveLength` functions are from vitest
+      // they will fail the test if the condition is not met
+      expect(actual).toHaveLength(expected.length);
+      for (const i in expected) {
+        // likewise, the `toBe` function will fail the test
+        // if the values are not equal
+        expect(actual[i]).toBe(expected[i]);
+      }
+    });
+  },
+);
